6b5d5a45e52deeea9940788505e975264a34c8d2
Ryota Inakazu <mail.to.inakazu@gmail.com>
2019-07-23 18:53:24 +0900

    feat: basic authentication

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,4 +1,10 @@
 RailsAdmin.config do |config|
+  # Basic Authentication
+  config.authenticate_with do
+    authenticate_or_request_with_http_basic('Site Message') do |username, password|
+      username == 'foo' && password == 'bar'
+    end
+  end
 
   ### Popular gems integration
 
